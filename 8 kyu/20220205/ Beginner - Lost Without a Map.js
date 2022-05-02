155736e85a323134647a65245aec1c46cb3be9b0
Lucas Baungartner <lucasbaungartner@protonmail.com>
2022-05-02 13:47:15 -0300

    Coding challenge and solution for 02/05/2022.

---
+++ 8 kyu/20220205/ Beginner - Lost Without a Map.js
@@ -0,0 +1,11 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x) {
+  return x.map((a) => a * 2);
+}
+
+//In this case, the map() method creates a new array with the results of the multiplication of every element on the initial array.
