40b6861b80930f165a7ece30da011e8ab513325e
Lucas Baungartner <lucasbaungartner@protonmail.com>
2022-05-02 12:10:19 -0300

    Coding challenge and solution for 02/05/2022.

---
+++ 8 kyu/Multiply.js
@@ -0,0 +1,11 @@
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//   a * b
+// }
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// It doesn't executed properly because it lacked something to do with the result. Now it returns it.
