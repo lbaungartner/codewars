977ac5ae27382a493281bac4f9c9aa04d8d1d483
lbaungartner <lucasbaungartner@protonmail.com>
2022-08-07 13:48:35 -0300

    Coding challenge done on 07/08/2022.

---
+++ 7 kyu/Printer Errors.js
@@ -0,0 +1,43 @@
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+// Examples:
+
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+function printerError(s) {
+  var err = 0;
+  let split = s.split("");
+  for (let i = 0; i < split.length; i++) {
+    if (
+      split[i] === "n" ||
+      split[i] === "o" ||
+      split[i] === "p" ||
+      split[i] === "q" ||
+      split[i] === "r" ||
+      split[i] === "s" ||
+      split[i] === "t" ||
+      split[i] === "u" ||
+      split[i] === "v" ||
+      split[i] === "w" ||
+      split[i] === "x" ||
+      split[i] === "y" ||
+      split[i] === "z"
+    ) {
+      var err = err + 1;
+    } else {
+      var err = err + 0;
+    }
+  }
+  return String(err) + "/" + String(split.length);
+}
