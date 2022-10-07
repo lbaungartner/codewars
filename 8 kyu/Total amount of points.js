c7620486e84bbbe81f0d17d54e6cf4d18724be9f
Lucas Baungartner <lucasbaungartner@protonmail.com>
2022-10-07 02:12:12 -0300

    Coding challenge done on 07/10/2022.

---
+++ 8 kyu/Total amount of points.js
@@ -0,0 +1,32 @@
+// Our football team has finished the championship.
+
+// Our teams match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x us our teams score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+  let result = 0;
+  for (let i = 0; i < games.length; i++) {
+    if (Number(games[i][0]) > Number(games[i][2])) {
+      result = result + 3;
+    } else if (Number(games[i][0]) == Number(games[i][2])) {
+      result = result + 1;
+    } else {
+      result = result + 0;
+    }
+  }
+  return result;
+}
